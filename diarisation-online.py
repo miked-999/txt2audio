76c3c5abbf5830509bf5b8ec40c72e81012cdabc
Michael <michael@Michaels-MacBook-Pro.local>
2024-07-01 22:09:26 +0100

    Committer: Michael <michael@Michaels-MacBook-Pro.local>  On branch main  Your branch is up to date with 'origin/main'.  Changes to be committed: 	modified:   config2.yaml 	modified:   diarisation-online.py

--- diarisation-online.py
+++ diarisation-online.py
@@ -13,5 +13,25 @@ offline_model = Model.from_pretrained(local_model_path+"/pytorch_model.bin")
 for weights, offline_weights in zip(model.parameters(), offline_model.parameters()):
     assert torch.equal(weights, offline_weights)
 
-offline_vad = Pipeline.from_pretrained("./config.yaml")
+
+
+#import torchaudio
+#waveform, sample_rate = torchaudio.load("./data/test1.wav")
+#audio_in_memory = {"waveform": waveform, "sample_rate": sample_rate}
+
+offline_vad = Pipeline.from_pretrained("./config2.yaml")
+#yaml file updated to point to the pytorch model file (segmentation)
+
+
+diarization = offline_vad("./data/test1.wav")
+
+#diarization =pipeline("./data/test1.wav")
+#pipeline = Pipeline.from_pretrained("pyannote/speaker-diarization-3.1", use_auth_token="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")
+
+with open("output/diarization.rttm", "w") as rttm:
+    diarization.write_rttm(rttm)
+
+# just checking output is the same
+#assert (diarization == offline_vad(audio_in_memory))
+
 
